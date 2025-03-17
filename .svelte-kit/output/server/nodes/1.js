00982c5f1557ca35a0b33d0ead45893e9b5f6c97
f14my <f14mygd@gmail.com>
2025-03-17 21:16:15 +0300

    Delete .svelte-kit directory

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.DBl_Q6hL.js","_app/immutable/chunks/CF65VEqF.js","_app/immutable/chunks/CGsa1yLP.js","_app/immutable/chunks/BhHcO2Ko.js","_app/immutable/chunks/C4cqjtjP.js","_app/immutable/chunks/QpZMwzuD.js","_app/immutable/chunks/OOUnWxbB.js","_app/immutable/chunks/DtjZshCJ.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.BND8InNl.js","_app/immutable/chunks/CF65VEqF.js","_app/immutable/chunks/CGsa1yLP.js","_app/immutable/chunks/BhHcO2Ko.js","_app/immutable/chunks/C4cqjtjP.js","_app/immutable/chunks/BObs6yVX.js","_app/immutable/chunks/DtjZshCJ.js","_app/immutable/chunks/D5icihEC.js","_app/immutable/chunks/QpZMwzuD.js"];
-export const stylesheets = ["_app/immutable/assets/2.BBnldZ1L.css","_app/immutable/assets/Menu.DwVcByan.css"];
-export const fonts = [];
